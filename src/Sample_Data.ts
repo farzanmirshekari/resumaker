2882bb29a200644942d2817e0dd9175ea06e0052
Farzan Mirshekari <farzanmirshekari@gmail.com>
2023-06-11 14:20:05 -0400

    Update default data to boilerplate information..

--- src/Sample_Data.ts
+++ src/Sample_Data.ts
@@ -1,64 +1,61 @@
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid';
 
 const data = {
     personal_details: {
-        full_name: 'Farzan Mirshekari',
-        phone_number: '+1 (234) 567-8910',
-        email_address: 'fmirshek@uwaterloo.ca',
-        github_username: 'farzanmirshekari',
-        linkedin_username: 'farzanmirshekari',
-        location: 'Waterloo, ON',
+        full_name: 'John Doe',
+        phone_number: '+1 (123) 456-7890',
+        email_address: 'johndoe@example.com',
+        github_username: 'johndoe',
+        linkedin_username: 'johndoe',
+        location: 'City, State',
     },
     skills: {
-        programming_languages: 'Java, JavaScript, TypeScript, HTML/CSS, C, C++',
-        frameworks: 'React, Bootstrap, Express.js',
-        tools: 'Webpack, Git, GitHub, Visual Studio, VSCode, Firebase, Heroku, MongoDB, Node.js, Guava, Arduino',
-        certifications: 'Fortinet Security Associate 1',
+        programming_languages: 'Java, Python, C++, SQL',
+        frameworks: 'Spring, Django',
+        tools: 'Git, Eclipse, MySQL',
+        certifications: 'AWS Certified Solutions Architect',
     },
-    projects: [
-        {
-            id: uuidv4(),
-            title: 'Résumaker',
-            overview:
-                'React application for generating stylish, detailed and versatile CVs which was used to make this résumé',
-            github_repository:
-                'https://github.com/farzanmirshekari/resume-builder',
-            tools: 'React, TypeScript, JavaScript, HTML/CSS',
-            start_date: 'April 2021',
-            end_date: 'Present',
-            details: [
-                'Created TypeScript interface models for each category and used export functions to return the proper structure of information based on user requests.',
-                'Used state and props to store user input, render live changes and display them on a preview panel.',
-                'Utilized CSS styling to create a professional, complete and robust template, which can be printed to PDF upon completion.',
-            ],
-        },
-    ],
     experience: [
         {
             id: uuidv4(),
-            position: 'IT and Help Desk Intern',
-            company: 'Woodgreen Community Services',
-            overview:
-                'Volunteered for 80+ hours with the IT department of a non-profit',
-            start_date: 'March 2022',
-            end_date: 'April 2022',
+            position: 'Software Engineer',
+            company: 'XYZ Corporation',
+            overview: 'Contributed to the development of enterprise software solutions',
+            start_date: 'April 2020',
+            end_date: 'Present',
             details: [
-                'Worked closely with the help desk with online, over the phone and in-person troubleshooting, solving a wide range of user issues.',
-                'Took on additional responsibility with the purchase, configuration and deployment of company devices.',
-                'Assisted the IT department with inventory management as well as writing and organizing tickets.',
+                'Collaborated with cross-functional teams to deliver scalable and reliable software',
+                'Participated in the full software development lifecycle, including requirement gathering, design, implementation, and testing',
+                'Identified and resolved software defects and performance issues',
+            ],
+        },
+    ],
+    projects: [
+        {
+            id: uuidv4(),
+            title: 'Inventory Management System',
+            overview: 'Developed a web-based inventory management system for a retail company',
+            github_repository: 'https://github.com/johndoe/inventory-system',
+            tools: 'Java, Spring Boot, MySQL',
+            start_date: 'January 2022',
+            end_date: 'March 2022',
+            details: [
+                'Designed and implemented the backend using Spring Boot framework',
+                'Created and optimized database schema using MySQL',
+                'Implemented RESTful APIs for data retrieval and manipulation',
             ],
         },
     ],
     education: [
         {
             id: uuidv4(),
-            education_institute: 'University of Waterloo',
-            program: 'Candidate for BASc. - Electrical Engineering',
-            start_date: '',
-            end_date: '',
-            details: ["1A Term's Dean Honours List - Grade: 90"],
+            education_institute: 'University of ABC',
+            program: 'Bachelor of Science in Computer Science',
+            start_date: 'September 2016',
+            end_date: 'May 2020',
+            details: ["Completed coursework in algorithms, data structures, and database management"],
         },
     ],
-}
+};
 
-export default data
+export default data;
